88dce9dd0b3b44ce2e0dc8f6b701aa8e279c0446
HanSome12138 <hsd2830815353@outlook.com>
2025-03-12 15:40:26 +0800

    delete original train

--- grokcso/models/tricks/__init__.py
+++ grokcso/models/tricks/__init__.py
@@ -1,8 +1,6 @@
-from .att_ista_basic import att_BasicBlock
 from .common import ResidualBlock_noBN, EdgeConv, default_conv, Upsampler
 from .ops import UpsampleBlock, _UpsampleBlock, BasicConv2d, FEB, FE
 __all__ = [
-    'att_BasicBlock',
     'ResidualBlock_noBN',
     'EdgeConv',
     'default_conv',
