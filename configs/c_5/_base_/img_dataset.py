b060ccbf1cc6f0a235a1677d826160c0611b5d0e
Mengze Xu~ <15028885+mengzexu@user.noreply.gitee.com>
2025-12-06 21:59:09 +0800

    fix bugs when computing c_5/c_7 PSNR and SSIM

--- configs/c_5/_base_/img_dataset.py
+++ configs/c_5/_base_/img_dataset.py
@@ -3,7 +3,8 @@ _base_ = '../../_base_/datasets/img_dataset.py'
 dataset_type = 'NoiseDataset'
 
 train_data_root = 'train'
-
+val_data_root = 'val'
+test_data_root = 'test'
 
 train_dataloader = dict(
   batch_size=64,
@@ -18,11 +19,42 @@ train_dataloader = dict(
   )
 )
 
+val_dataloader = dict(
+  batch_size=64,
+  num_workers=2,
+  pin_memory=True,
+  sampler=dict(type='DefaultSampler', shuffle=True),
+  dataset=dict(
+    type=dataset_type,
+    data_root=val_data_root,
+    length=10000,
+    c=7
+  ),
+  drop_last=True
+)
+
+test_dataloader = dict(
+  batch_size=64,
+  num_workers=2,
+  pin_memory=True,
+  sampler=dict(type='DefaultSampler', shuffle=False),
+  dataset=dict(
+    type=dataset_type,
+    data_root=test_data_root,
+    length=10000,
+    c=7
+  ),
+  drop_last=True
+)
+
 
 val_evaluator = [
   dict(
     type="CSO_Metrics",
     brightness_threshold=50,
+    c=5),
+  dict(
+    type="Similarity",
     c=5),
 ]
 test_evaluator = val_evaluator
--- configs/c_7/_base_/img_dataset.py
+++ configs/c_7/_base_/img_dataset.py
@@ -1,6 +1,8 @@
 _base_ = '../../_base_/datasets/img_dataset.py'
 dataset_type = 'NoiseDataset'
 train_data_root = 'train'
+val_data_root = 'val'
+test_data_root = 'test'
 
 train_dataloader = dict(
   batch_size=64,
@@ -15,11 +17,42 @@ train_dataloader = dict(
   )
 )
 
+val_dataloader = dict(
+  batch_size=64,
+  num_workers=2,
+  pin_memory=True,
+  sampler=dict(type='DefaultSampler', shuffle=True),
+  dataset=dict(
+    type=dataset_type,
+    data_root=val_data_root,
+    length=10000,
+    c=7
+  ),
+  drop_last=True
+)
+
+test_dataloader = dict(
+  batch_size=64,
+  num_workers=2,
+  pin_memory=True,
+  sampler=dict(type='DefaultSampler', shuffle=False),
+  dataset=dict(
+    type=dataset_type,
+    data_root=test_data_root,
+    length=10000,
+    c=7
+  ),
+  drop_last=True
+)
+
 
 val_evaluator = [
   dict(
     type="CSO_Metrics",
     brightness_threshold=50,
+    c=7),
+  dict(
+    type="Similarity",
     c=7),
 ]
 test_evaluator = val_evaluator
